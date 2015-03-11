f9dcb8f83cae305093e04598f5fd8cc16a231b77
koue <koue@chaosophia.net>
2015-03-11 11:38:05 +0200

    Fix linux compilation.

--- index.c
+++ index.c
@@ -1,7 +1,6 @@
 /*
- *
  * Copyright (c) 2004-2006 Daniel Hartmeier. All rights reserved.
- * Copyright (c) 2012, 2013 Nikola Kolev. All rights reserved.
+ * Copyright (c) 2012-2015 Nikola Kolev. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,8 +30,8 @@
 
 #include <sys/file.h>
 #include <sys/stat.h>
-#include <sys/time.h>
 #include <sys/types.h>
+#include <time.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdarg.h>
