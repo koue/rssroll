b0af0ba4aef3c9108947b14c4af67dad4bde2e74
Nikola Kolev <koue@chaosophia.net>
2018-08-16 16:06:35 +0000

    Add RSS 1.0 test

--- tests/rssroll.sh
+++ tests/rssroll.sh
@@ -5,5 +5,6 @@ sqlite3 rssrolltest.db "INSERT INTO categories (title) VALUES ('test')"
 sqlite3 rssrolltest.db "INSERT INTO channels (catid, link) VALUES (1, 'https://raw.githubusercontent.com/koue/rssroll/develop/tests/atom.xml')"
 sqlite3 rssrolltest.db "INSERT INTO channels (catid, link) VALUES (1, 'https://raw.githubusercontent.com/koue/rssroll/develop/tests/rss091.xml')"
 sqlite3 rssrolltest.db "INSERT INTO channels (catid, link) VALUES (1, 'https://raw.githubusercontent.com/koue/rssroll/develop/tests/rss092.xml')"
+sqlite3 rssrolltest.db "INSERT INTO channels (catid, link) VALUES (1, 'https://raw.githubusercontent.com/koue/rssroll/develop/tests/rss10.xml')"
 sqlite3 rssrolltest.db "INSERT INTO channels (catid, link) VALUES (1, 'https://raw.githubusercontent.com/koue/rssroll/develop/tests/rss20.xml')"
 ../src/rssroll -v -d rssrolltest.db
